a131c9a426ce990841b19f9dfbbb7cfef789232e
Shivam Mishra <96122065+Welcome21997@users.noreply.github.com>
2022-02-18 14:45:18 +0530

    Add files via upload
    EmployeesController is C# file accept incoming request from the Hypertext Transfer Protocol (Http) and passes this request to the interface...

---
+++ EmployeesController.cs
@@ -0,0 +1,114 @@
+﻿using DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyApplication.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await _employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error in retriving data from database");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            try
+            {
+                var result = await _employeeRepository.GetEmployee(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error in retriving data from database");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            try
+            {
+                if (employee == null)
+                {
+                    return BadRequest();
+                }
+                var CreatedEmployee= await _employeeRepository.AddEmployee(employee);
+                return CreatedAtAction(nameof(GetEmployee), new { id = CreatedEmployee.Id }, CreatedEmployee);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error in retriving data from database");
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id,Employee employee)
+        {
+            try
+            {
+                if (id != employee.Id)
+                {
+                    return BadRequest("Id Mismatch");
+                }
+                var EmployeeUpdate = await _employeeRepository.GetEmployee(id);
+                if (EmployeeUpdate == null)
+                {
+                    return NotFound($"Employee Id ={id} Not Found");
+                }
+                return await _employeeRepository.UpdateEmployee(employee);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error in retriving data from database");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(int id)
+        {
+            try
+            {
+               
+                var EmployeeDelete = await _employeeRepository.GetEmployee(id);
+                if (EmployeeDelete == null)
+                {
+                    return NotFound($"Employee Id ={id} Not Found");
+                }
+                return await _employeeRepository.DeleteEmployee(id);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error in retriving data from database");
+            }
+        }
+    }
+}
+
