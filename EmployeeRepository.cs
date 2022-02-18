5aa17e83e2d37d775d29ce3ba89cc2dc471a3927
Shivam Mishra <96122065+Welcome21997@users.noreply.github.com>
2022-02-18 14:57:19 +0530

    Add files via upload
    Getting request from IEmployeeRepository that method will execute which requested by the client..... If it is valid request then this method pass though the data base and database will response to client. if request is not valid then it will provide error message..

---
+++ EmployeeRepository.cs
@@ -0,0 +1,62 @@
+﻿using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using MyApplication.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApplication.Repository
+{
+    public class EmployeeRepository:IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Employee> AddEmployee(Employee employee)
+        {
+            var result = await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Employee> DeleteEmployee(int Id)
+        {
+            var result = await _context.Employees.FirstOrDefaultAsync(a => a.Id == Id);
+            if (result != null)
+            {
+                _context.Employees.Remove(result);
+                await _context.SaveChangesAsync();
+                return result;
+            }
+            return null;
+        }
+
+        public async Task<Employee> GetEmployee(int Id)
+        {
+            return await _context.Employees.FirstOrDefaultAsync(a => a.Id == Id);
+
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> UpdateEmployee(Employee employee)
+        {
+            var result = await _context.Employees.FirstOrDefaultAsync(a => a.Id == employee.Id);
+            if (result != null)
+            {
+                result.Name = employee.Name;
+                result.City = employee.City;
+                await _context.SaveChangesAsync();
+                return result;
+            }
+            return null;
+        }
+    }
+}
