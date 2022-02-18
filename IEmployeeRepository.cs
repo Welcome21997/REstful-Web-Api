068e10e2dcc4e09630418dbf79fe439bd06acce6
Shivam Mishra <96122065+Welcome21997@users.noreply.github.com>
2022-02-18 14:52:57 +0530

    Add files via upload
    IEmployeeRepository is a interface in C# in which only method declaration not implementation.... when we inherit interface then it is responsibility of child class to implement all the method which is defined inside interface ... After getting request from the controller interface will pass the request to EmployeeRepository C# class file.

---
+++ IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApplication.Repository
+{
+   public interface IEmployeeRepository
+    {
+        public Task<Employee> AddEmployee(Employee employee);
+        public Task<Employee> DeleteEmployee(int Id);
+        public Task<Employee> GetEmployee(int Id);
+        public Task<IEnumerable<Employee>> GetEmployees();
+        public Task<Employee> UpdateEmployee(Employee employee);
+
+    }
+}
